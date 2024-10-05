7a3ebb28a51581e991505285dec953104a887dfe
to'raboyev xursand <xursandtoraboyev810@gmail.com>
2024-10-06 03:13:19 +0500

    started

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Taxi from "./pages/taxi"
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/taxi" element={<Taxi />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
